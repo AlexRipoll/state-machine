d474a6efec4cc687306d673c753b8211814568ff
Alex Ripoll <alexripollpiera@gmail.com>
2024-07-16 20:02:07 +0200

    make balance mod generic

--- src/balances.rs
+++ src/balances.rs
@@ -1,18 +1,21 @@
 use std::collections::BTreeMap;
 
-pub type AccountID = String;
-pub type Balance = u128;
+use num::{CheckedAdd, CheckedSub, Zero};
 
 /// This is the Balances Module.
 /// It is a simple module which keeps track of how much balance each account has in this state
 /// machine.
 #[derive(Debug)]
-pub struct Pallet {
+pub struct Pallet<AccountId, Balance> {
     // A simple storage mapping from accounts (`String`) to their balances (`u128`).
-    balances: BTreeMap<AccountID, Balance>,
+    balances: BTreeMap<AccountId, Balance>,
 }
 
-impl Pallet {
+impl<AccountId, Balance> Pallet<AccountId, Balance>
+where
+    AccountId: Ord + Clone,
+    Balance: Zero + CheckedSub + CheckedAdd + Copy,
+{
     /// Create a new instance of the balances module.
     pub fn new() -> Self {
         Self {
@@ -21,14 +24,14 @@ impl Pallet {
     }
 
     /// Set the balance of an account `who` to some `amount`.
-    pub fn set_balance(&mut self, who: &AccountID, amount: Balance) {
+    pub fn set_balance(&mut self, who: &AccountId, amount: Balance) {
         self.balances.insert(who.clone(), amount);
     }
 
     /// Get the balance of an account `who`.
     /// If the account has no stored balance, we return zero.
-    pub fn balance(&self, who: &AccountID) -> Balance {
-        *self.balances.get(who).unwrap_or(&0)
+    pub fn balance(&self, who: &AccountId) -> Balance {
+        *self.balances.get(who).unwrap_or(&Balance::zero())
     }
 
     /// Transfer `amount` from one account to another.
@@ -36,19 +39,19 @@ impl Pallet {
     /// and that no mathematical overflows occur.
     pub fn transfer(
         &mut self,
-        caller: AccountID,
-        to: AccountID,
+        caller: AccountId,
+        to: AccountId,
         amount: Balance,
     ) -> Result<(), &'static str> {
         let caller_balance = self.balance(&caller);
         let to_balance = self.balance(&to);
 
         let new_caller_balance = caller_balance
-            .checked_sub(amount)
+            .checked_sub(&amount)
             .ok_or("insufficient balance")?;
 
         let new_to_balance = to_balance
-            .checked_add(amount)
+            .checked_add(&amount)
             .ok_or("max founds limit reached")?;
 
         self.set_balance(&caller, new_caller_balance);
@@ -64,7 +67,7 @@ mod test {
 
     #[test]
     fn init_balances() {
-        let mut balances = Pallet::new();
+        let mut balances = Pallet::<String, u128>::new();
 
         assert_eq!(balances.balance(&"alice".to_string()), 0);
 
@@ -73,9 +76,10 @@ mod test {
         assert_eq!(balances.balance(&"alice".to_string()), 100);
         assert_eq!(balances.balance(&"bob".to_string()), 0);
     }
+
     #[test]
     fn transfer_balance() {
-        let mut balances = Pallet::new();
+        let mut balances = Pallet::<String, u128>::new();
 
         assert_eq!(
             balances.transfer("alice".to_string(), "bob".to_string(), 10),
