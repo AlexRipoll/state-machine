1e2653c76a180a45e0e16fee8b68d3b1dccc4d4f
Alex Ripoll <alexripollpiera@gmail.com>
2024-07-16 10:07:41 +0200

    add balance transfer method

--- src/balances.rs
+++ src/balances.rs
@@ -1,4 +1,7 @@
-use std::collections::BTreeMap;
+use std::{
+    collections::BTreeMap,
+    ops::{Add, Sub},
+};
 
 #[derive(Debug)]
 struct Pallet {
@@ -18,6 +21,32 @@ impl Pallet {
 
     pub fn balance(&self, who: &String) -> u128 {
         *self.balances.get(who).unwrap_or(&0)
+    }
+
+    /// Transfer `amount` from one account to another.
+    /// This function verifies that `from` has at least `amount` balance to transfer,
+    /// and that no mathematical overflows occur.
+    pub fn transfer(
+        &mut self,
+        caller: String,
+        to: String,
+        amount: u128,
+    ) -> Result<(), &'static str> {
+        let caller_balance = self.balance(&caller);
+        let to_balance = self.balance(&to);
+
+        let new_caller_balance = caller_balance
+            .checked_sub(amount)
+            .ok_or("insufficient balance")?;
+
+        let new_to_balance = to_balance
+            .checked_add(amount)
+            .ok_or("max founds limit reached")?;
+
+        self.set_balance(&caller, new_caller_balance);
+        self.set_balance(&to, new_to_balance);
+
+        Ok(())
     }
 }
 
