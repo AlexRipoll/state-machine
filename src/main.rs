02aac75dafef41f50841bdc480ff182b9c0dfe72
Alex Ripoll <alexripollpiera@gmail.com>
2024-07-17 13:03:21 +0200

    add execute_block runtime method

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use support::{Block, Dispatch};
 use types::{AccountId, Balance, BlockNumber, Nonce};
 
 mod balances;
+mod support;
 mod system;
 
 mod types {
@@ -8,6 +10,15 @@ mod types {
     pub type Balance = u128;
     pub type BlockNumber = u32;
     pub type Nonce = u32;
+    pub type Extrinsic = crate::support::Extrinsic<AccountId, crate::RuntimeCall>;
+    pub type Header = crate::support::Header<BlockNumber>;
+    pub type Block = crate::support::Block<Header, Extrinsic>;
+}
+
+// These are all the calls which are exposed to the world.
+// Note that it is just an accumulation of the calls exposed by each module.
+pub enum RuntimeCall {
+    // TODO: Not implemented yet.
 }
 
 #[derive(Debug)]
@@ -32,6 +43,47 @@ impl Runtime {
             system: system::Pallet::new(),
             balances: balances::Pallet::new(),
         }
+    }
+
+    // Execute a block of extrinsics. Increments the block number.
+    fn execute_block(&mut self, block: types::Block) -> support::DispatchResult {
+        self.system.inc_block_number();
+        if self.system.block_number() != block.header.block_number {
+            return Err("block number mistmatch");
+        }
+
+        block
+            .extrinsics
+            .into_iter()
+            .enumerate()
+            .for_each(|(i, extrinsic)| {
+                self.dispatch(extrinsic.caller, extrinsic.call)
+                    .map_err(|e| {
+                        eprintln!(
+                        "Extrinsic Error\n\tBlock Number: {}\n\tExtrinsic Number: {}\n\tError: {}",
+                        block.header.block_number, i, e
+                    )
+                    });
+            });
+
+        Ok(())
+    }
+}
+
+impl crate::support::Dispatch for Runtime {
+    type Caller = <Runtime as system::Config>::AccountId;
+    type Call = RuntimeCall;
+    // Dispatch a call on behalf of a caller. Increments the caller's nonce.
+    //
+    // Dispatch allows us to identify which underlying module call we want to execute.
+    // Note that we extract the `caller` from the extrinsic, and use that information
+    // to determine who we are executing the call on behalf of.
+    fn dispatch(
+        &mut self,
+        caller: Self::Caller,
+        runtime_call: Self::Call,
+    ) -> support::DispatchResult {
+        unimplemented!();
     }
 }
 
